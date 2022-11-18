a8da3438870222daf1c224705e5f1b4b0c520991
zisami <sascha.zika@gmx.at>
2022-11-18 18:10:05 +0100

    test Evenst

--- src/lib/paper/paper-canvas.js
+++ src/lib/paper/paper-canvas.js
@@ -7,9 +7,13 @@ export default function newPaperCanvas(node) {
 	// Create an empty project and a view for the canvas:
 	paper.setup(canvas);
 	console.log(paper.view);
+	var myPath = new paper.Path();
+	myPath.strokeColor = 'black';
 
 	createBackground();
 	createEditor();
+	createCircle();
+
 	// Draw the view now:
 	paper.view.draw();
 }
@@ -18,6 +22,7 @@ function createBackground() {
 	const pathRect = new paper.Path.Rectangle(rectangle);
 	pathRect.fillColor = '#eee';
 }
+
 function createEditor(settings = {}) {
 	if (!settings?.margin) {
 		settings.margin = 10;
@@ -31,4 +36,17 @@ function createEditor(settings = {}) {
 	);
 	const pathEditor = new paper.Path.Rectangle(rectangle);
 	pathEditor.fillColor = '#fff';
+}
+
+function createCircle() {
+	const circle = new paper.Path.Circle({
+		center: paper.view.center,
+		radius: 25,
+		fillColor: 'black'
+	});
+	circle.strokeColor = 'black';
+	circle.onMouseDown = function (event) {
+		console.log(event);
+		this.fillColor = 'red';
+	};
 }
