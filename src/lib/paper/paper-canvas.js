1bed2e4fafbf80d5387ac5e5b5ecaa8947fa14e0
zisami <sascha.zika@gmx.at>
2022-11-18 14:21:48 +0100

    Editor Pane working

--- src/lib/paper/paper-canvas.js
+++ src/lib/paper/paper-canvas.js
@@ -6,22 +6,29 @@ export default function newPaperCanvas(node) {
 
 	// Create an empty project and a view for the canvas:
 	paper.setup(canvas);
-	console.log(paper.view.viewSize);
-	const rectangle = new paper.Rectangle(new paper.Point(0, 0), new paper.Point(150, 100));
-	const pathRect = new paper.Path.Rectangle(rectangle);
-	pathRect.fillColor = '#e9e9ff';
-	//pathRect.selected = true;
+	console.log(paper.view);
 
-	// Create a Paper.js Path to draw a line into it:
-	const path = new paper.Path();
-	// Give the stroke a color
-	path.strokeColor = 'black';
-	const start = new paper.Point(100, 100);
-	// Move to start and draw a line from there
-	path.moveTo(start);
-	// Note that the plus operator on Point objects does not work
-	// in JavaScript. Instead, we need to call the add() function:
-	path.lineTo(start.add([200, 250]));
+	createBackground();
+	createEditor();
 	// Draw the view now:
 	paper.view.draw();
+}
+function createBackground() {
+	const rectangle = new paper.Rectangle(new paper.Point(0, 0), paper.view.viewSize);
+	const pathRect = new paper.Path.Rectangle(rectangle);
+	pathRect.fillColor = '#eee';
+}
+function createEditor(settings = {}) {
+	if (!settings?.margin) {
+		settings.margin = 10;
+	}
+	const rectangle = new paper.Rectangle(
+		new paper.Point(settings.margin, settings.margin),
+		new paper.Point(
+			paper.view.viewSize.width - settings.margin,
+			paper.view.viewSize.height - settings.margin
+		)
+	);
+	const pathEditor = new paper.Path.Rectangle(rectangle);
+	pathEditor.fillColor = '#fff';
 }
