9916c5f44304bc50e057be800cac7dede7d7e2dd
zisami <sascha.zika@gmx.at>
2022-11-25 20:56:08 +0100

    Renders Somethin from the SVG

--- src/lib/product-viewer/product-viewer.svelte
+++ src/lib/product-viewer/product-viewer.svelte
@@ -5,13 +5,16 @@
 
 	import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader.js';
 	import sheep from '$lib/assets/img/sheep.svg';
-	import { ShapeGeometry } from 'three';
+	import { ExtrudeGeometry, Group, ShapeGeometry } from 'three';
 	const scale = spring(1);
 	console.log(sheep);
 
 	const loader = useLoader(SVGLoader, () => new SVGLoader());
-	loader.load(sheep, (obj) => {
-		console.log(obj);
+	let shapes;
+	loader.load(sheep, (svg) => {
+		console.log(svg);
+		shapes = svg.paths.map((path) => path.toShapes(true));
+		console.log(shapes);
 	});
 </script>
 
@@ -20,18 +23,33 @@
 -->
 <Canvas>
 	<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-		<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={false} target={{ y: 1 }} />
+		<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={true} target={{ y: 1 }} />
 	</T.PerspectiveCamera>
 
 	<T.DirectionalLight castShadow position={[3, 10, 10]} />
 	<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
 	<T.AmbientLight intensity={0.2} />
 	<T.DirectionalLight position={[3, -10, 10]} intensity={0.2} />
+
 	<!-- sheep -->
-	<T.Mesh position.y={1} castShadow let:ref>
-		<!-- <T.ShapeGeometry args={[sheep]} /> -->
-		<T.MeshStandardMaterial color="hotpink" />
-	</T.Mesh>
+	<T.Group scale={1}>
+		<T.Mesh position.y={1} castShadow let:ref>
+			{#if shapes}
+				{#each shapes as shapesArray}
+					{#each shapesArray as shape, index}
+						{console.log(shape)}
+						<T.ExtrudeGeometry
+							args={shape}
+							depth={2}
+							bevelEnabled={false}
+							position={[0, 1 + index, 0]}
+						/>
+					{/each}
+				{/each}
+			{/if}
+			<T.MeshStandardMaterial color="hotpink" />
+		</T.Mesh>
+	</T.Group>
 	<!-- Floor -->
 	<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
 		<T.CircleGeometry args={[3, 72]} />
