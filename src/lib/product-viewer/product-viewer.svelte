23ccd84a45fd750bf5b0a70f78b55c17be6e58c7
zisami <sascha.zika@gmx.at>
2022-11-26 22:51:44 +0100

    basic world

--- src/lib/product-viewer/product-viewer.svelte
+++ src/lib/product-viewer/product-viewer.svelte
@@ -4,50 +4,74 @@
 	import { degToRad } from 'three/src/math/MathUtils';
 
 	import { SVGLoader } from 'three/examples/jsm/loaders/SVGLoader.js';
-	import { ExtrudeGeometry, Group, ShapeGeometry } from 'three';
-	const scale = spring(1);
+	import { Float } from '@threlte/extras'
+
+
+	const scale = spring(0.001);
 	const loader = useLoader(SVGLoader, () => new SVGLoader());
 	let shapesFromPaths;
 	loader.load('/src/lib/assets/img/sheep.svg', (svg) => {
-		console.log('svg', svg);
 		shapesFromPaths = svg.paths.map((path) => SVGLoader.createShapes(path));
-		console.log(shapesFromPaths);
 	});
-	const colors =['hotpink', 'green']
-	let extrudedSvg
-	$: extrudedSvg?.center()
-	
+	const colors =['white', 'black']
+
+	const model ={
+		position: [0.75, 1.5, 0],
+	}
 </script>
 
 <Canvas>
-	<T.PerspectiveCamera makeDefault position={[10, 10, 10]} fov={24}>
-		<OrbitControls maxPolarAngle={degToRad(80)} enableZoom={true} target={{ y: 1 }} />
+	<T.PerspectiveCamera makeDefault position={[8, 2, 8]}  fov={20}>
+		<OrbitControls 
+		minPolarAngle={degToRad(50)} 
+		maxPolarAngle={degToRad(80)} 
+		minAzimuthAngle={degToRad(-30)} 
+		maxAzimuthAngle={degToRad(90)} 
+		enableZoom={true} 
+		maxDistance={150} 
+		target={model.position}
+		enableDamping/>
 	</T.PerspectiveCamera>
 
-	<T.DirectionalLight castShadow position={[3, 10, 10]} />
-	<T.DirectionalLight position={[-3, 10, -10]} intensity={0.2} />
-	<T.AmbientLight intensity={0.2} />
-	<T.DirectionalLight position={[3, -10, 10]} intensity={0.2} />
+	<T.DirectionalLight castShadow position={[-1, 10, 10]} />
 
+	<T.AmbientLight intensity={0.2} />
+
+	<Float speed={1}  >
 	<!-- sheep -->
-	<T.Group scale={0.001*-1} position={[0.5, 2, 0]} >
+	<T.Group scale={$scale*-1} position={model.position}  rotation.y={degToRad(35)} >
 		{#if shapesFromPaths}
 			{#each shapesFromPaths as shapesArray, index}
 				{#each shapesArray as shape}
-					{console.log('shape',colors[index], shape)}
-					<T.Mesh castShadow let:ref bind:ref={extrudedSvg}>
-						<T.ExtrudeGeometry args={[shape, {depth:10}] } />
-						<T.MeshStandardMaterial color={colors[index]}/>
+					<T.Mesh castShadow let:ref>
+						<InteractiveObject
+							object={ref}
+							interactive
+							on:pointerdown={() => ($scale = 0.00125)}
+							on:pointerup={() => ($scale = 0.001)}
+						/>
+						<T.ExtrudeGeometry args={[shape, {depth:50}] } />
+						<T.MeshStandardMaterial color={colors[index]} />
 					</T.Mesh>
 						
 					{/each}
 			{/each}
 		{/if}
 	</T.Group>
+	
+
+</Float>
+
 
 	<!-- Floor -->
 	<T.Mesh receiveShadow rotation.x={degToRad(-90)}>
-		<T.CircleGeometry args={[3, 72]} />
-		<T.MeshStandardMaterial color="white" />
+		<T.CircleGeometry args={[10, 72]} />
+		<T.MeshStandardMaterial color="darkgreen" />
+	</T.Mesh>
+
+	<!-- Sky -->
+	<T.Mesh receiveShadow rotation.z={degToRad(-90)}>
+		<T.SphereGeometry args={[10, 36, 18]} />
+		<T.MeshStandardMaterial color="#87ceeb" side={1}/>
 	</T.Mesh>
 </Canvas>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,17 +1,15 @@
 <script>
 	import ProductViewer from "../lib/product-viewer/product-viewer.svelte";
-	import SvgTest from "../lib/assets/svg-test.svelte";
 </script>
 
 <div id="wrapper-product-viewer">
 	<ProductViewer />
-	<img src="/src/lib/assets/img/test.svg" alt="sheep" />
 </div>
 
 <style>
 	#wrapper-product-viewer {
-		height: 500px;
-		width: 100%;
+		height: 100vh;
+		width: 100vw;
 		border: solid 1px black;
 	}
 </style>
