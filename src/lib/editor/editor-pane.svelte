5d49bedc044fcf56994226c5afe2b6402cc12e30
zisami <sascha.zika@gmx.at>
2022-11-27 19:03:41 +0100

    editorPane functioning

---
+++ src/lib/editor/editor-pane.svelte
@@ -0,0 +1,28 @@
+<script>
+	import { fly } from 'svelte/transition';
+	import { state } from '$lib/stores/state.js';
+	function toggleEditorPane() {
+		$state.hideEditorPane = !$state.hideEditorPane;
+	}
+</script>
+
+{#if !$state.hideEditorPane}
+	<div
+		class="w-screen container absolute px-4 bottom-0 h-1/2 grid left-1/2 transform -translate-x-1/2 "
+		transition:fly={{ y: 200, duration: 800 }}
+	>
+		<div class=" bg-slate-100 rounded-t-2xl p-4 bottom-0 relative">
+			<div
+				class="rounded-full bg-green-700 absolute text-gray-200 center p-1 text-xs w-8 h-8 grid place-content-center font-bold -right-3 -mt-8 border-2 border-green-100 hover:cursor-pointer"
+				on:click={toggleEditorPane}
+				on:keydown={toggleEditorPane}
+			>
+				X
+			</div>
+			{$state.hideEditorPane}
+		</div>
+	</div>
+{/if}
+
+<style>
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,20 +1,15 @@
 <script>
-	import ProductViewer from "../lib/product-viewer/product-viewer.svelte";
-	import Logo from "../lib/logo.svelte";
-	import {state} from "$lib/stores/state.js"
+	import EditorPane from '$lib/editor/editor-pane.svelte';
+	import ProductViewer from '$lib/product-viewer/product-viewer.svelte';
+	import Logo from '$lib/logo.svelte';
 </script>
 
-<Logo/>
+<Logo />
 <div class="h-screen w-screen  relative">
 	<ProductViewer />
 </div>
 
-<div class="w-screen  absolute bottom-0 h-1/2 bg-slate-500" hidden={$state.hideEditorPane}>
-{$state.hideEditorPane}
-</div>
+<EditorPane />
 
 <style>
-
 </style>
-
-
