929685d6eae17e6cbb144de8395ba42e9990619c
zisami <sascha.zika@gmx.at>
2022-11-27 19:33:59 +0100

    added wrong loading logik :(

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,12 @@
 <script>
-  import "../app.css";
+	import { navigating } from '$app/stores';
+
+	import { loading } from '$lib/stores/loading';
+	import Loading from '$lib/page-loader/page-loader.svelte';
+	import '../app.css';
+
+  //Setting the Loading Store to the Navigationstate
+  $: $loading = !!$navigating
 </script>
 
 <slot />
