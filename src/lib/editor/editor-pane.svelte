6233afc590df2b91c6e6e0317e548fa5d10bcd69
zisami <sascha.zika@gmx.at>
2022-11-29 22:55:15 +0100

    sheep icons

--- src/lib/editor/editor-pane.svelte
+++ src/lib/editor/editor-pane.svelte
@@ -1,6 +1,8 @@
 <script>
 	import { fly } from 'svelte/transition';
 	import { state } from '$lib/stores/state.js';
+	import MdClose from 'svelte-icons/md/MdClose.svelte'
+
 	function toggleEditorPane() {
 		$state.hideEditorPane = !$state.hideEditorPane;
 	}
@@ -11,15 +13,15 @@
 		class="w-screen container absolute px-4 bottom-0 h-1/2 grid left-1/2 transform -translate-x-1/2 "
 		transition:fly={{ y: 200, duration: 800 }}
 	>
-		<div class=" bg-slate-100 rounded-t-2xl p-4 bottom-0 relative">
+		<div class=" bg-slate-100 rounded-t-2xl p-4 pb-0 bottom-0 relative">
 			<div
 				class="rounded-full bg-green-700 absolute text-gray-200 center p-1 text-xs w-8 h-8 grid place-content-center font-bold -right-3 -mt-8 border-2 border-green-100 hover:cursor-pointer"
 				on:click={toggleEditorPane}
 				on:keydown={toggleEditorPane}
 			>
-				X
+				<MdClose/>
 			</div>
-			{$state.hideEditorPane}
+			<slot />
 		</div>
 	</div>
 {/if}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,13 +2,25 @@
 	import EditorPane from '$lib/editor/editor-pane.svelte';
 	import ProductViewer from '$lib/product-viewer/product-viewer.svelte';
 	import Logo from '$lib/logo.svelte';
+	import headIconPath from '$lib/assets/icons/head.svg'
+	import bodyIconPath from '$lib/assets/icons/body.svg'
+	import skeletonIconPath from '$lib/assets/icons/skeleton.svg'
 </script>
 
 <Logo />
 
 <ProductViewer />
 
-<EditorPane />
+<EditorPane>
+	<div class="h-full flex flex-col justify-between bg-slate-300">
+		<div class="cotainer"></div>
+		<div class="container flex justify-center ">
+			<div class="w-12 h-12 bg-sky-600 rounded-t-lg mx-1"><img src={headIconPath} alt="Edit Head"></div>
+			<div class="w-12 h-12 bg-sky-600 rounded-t-lg mx-1"><img src={bodyIconPath} alt="Edit Body"></div>
+			<div class="w-12 h-12 bg-sky-600 rounded-t-lg mx-1"><img src={skeletonIconPath} alt="edit Skeleton"></div>
+		</div>
+	</div>
+</EditorPane>
 
 <style>
 </style>
